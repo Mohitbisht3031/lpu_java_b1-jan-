5411102a1fe1bc601acd0394159177dca9e8e5f6
unknown <imohiitsingh@gmail.com>
2024-01-15 20:52:25 +0530

    Day1

---
+++ DAy1/first.java
@@ -0,0 +1,28 @@
+package DAy1;
+import java.util.Scanner;
+
+/**
+ * Hey this is a first java code
+ */
+
+// write a prog to take 3 number i/p from the user and print there sum
+
+public class first {
+
+    public static void main(String[] args) {
+        // int s = args.length;
+        // if(s > 0){
+        //     for(int i = 0;i<s;i++)System.out.println(args[i]);
+        // }
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        sc.close();
+        // System.out.println(args[0]);
+        // System.out.println(args[1]);
+        // System.out.println(args[2]);
+        // System.out.println("Hey , welcome to the first java class"); //cout -> 
+    }    
+    
+}
