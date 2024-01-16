373326e1d181364e8dcbb0ad28231d21f6177339
unknown <imohiitsingh@gmail.com>
2024-01-16 20:55:05 +0530

    Day2

---
+++ Day2/second.java
@@ -0,0 +1,15 @@
+/**
+ * second leacture
+ */
+public class second {
+
+    public static void main(String[] args) {
+        Integer a = 10;
+        System.out.println("value of a is this "+a);
+        String str = "567";
+        a = Integer.parseInt(str);
+        System.out.println("value of a is this "+a);
+        // System.out.println(Integer.rotateLeft(a, 2));
+    }
+    
+}
