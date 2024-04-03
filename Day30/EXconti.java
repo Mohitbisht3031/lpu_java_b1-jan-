8cb50173963e66ad0332c0dde91e5ab6359a37dd
unknown <imohiitsingh@gmail.com>
2024-04-03 19:54:09 +0530

    Day30

---
+++ Day30/EXconti.java
@@ -0,0 +1,21 @@
+package Day30;
+
+import Day30.Sonu;
+
+public class EXconti {
+    int D(int v) throws Sonu{
+        // if(v == 0)throw new Sonu("Mere ilake me aane ka nhi !!");
+        assert v==0 : "Mere ilake me aane ka nhi !!";
+        return v;
+    }
+
+    public static void main(String[] args) {
+        EXconti obj = new EXconti();
+        try{
+            obj.D(0);
+        } catch (Sonu ex){
+            System.out.println("Sonu wants to say "+ex.getMessage());
+        }
+    }
+
+}
---
+++ Day30/Sonu.java
@@ -0,0 +1,7 @@
+package Day30;
+
+public class Sonu extends Exception{
+    public Sonu(String msg){
+        super(msg);
+    }
+}
